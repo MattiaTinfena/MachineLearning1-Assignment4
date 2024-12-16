4bcf788e2d4c0cd6689fe5b2915af73f6c8ac1eb
Mattia <mattia.tinfena@gmail.com>
2024-12-16 17:45:35 +0100

    complete code

---
+++ Task 1-2/main.m
@@ -0,0 +1,71 @@
+clear all
+close all
+
+%% TASK1
+
+load glass_dataset
+
+x = glassInputs;
+t = glassTargets;
+
+% Choose a Training Function
+% For a list of all training functions type: help nntrain
+% 'trainlm' is usually fastest.
+% 'trainbr' takes longer but may be better for challenging problems.
+% 'trainscg' uses less memory. Suitable in low memory situations.
+trainFcn = 'trainscg';  % Scaled conjugate gradient backpropagation.
+
+% Create a Pattern Recognition Network
+hiddenLayerSize = 10;
+net = patternnet(hiddenLayerSize, trainFcn);
+
+% Setup Division of Data for Training, Validation, Testing
+net.divideParam.trainRatio = 70/100;
+net.divideParam.valRatio = 15/100;
+net.divideParam.testRatio = 15/100;
+
+% Train the Network
+[net,tr] = train(net,x,t);
+
+% Test the Network
+y = net(x);
+e = gsubtract(t,y);
+performance = perform(net,t,y)
+tind = vec2ind(t);
+yind = vec2ind(y);
+percentErrors = sum(tind ~= yind)/numel(tind);
+
+% View the Network
+view(net)
+
+% Plots
+figure, plotperform(tr)
+figure, plottrainstate(tr)
+figure, ploterrhist(e)
+figure, plotconfusion(t,y)
+figure, plotroc(t,y)
+
+%% TASK 2
+
+% Select a random class from 1 to 10
+class1 = randi([1,10]);
+class2 = randi([1,10]);
+
+while class1 == class2
+    class2 = randi([1,10]);
+end
+
+% Load data
+[training_set, training_class]= loadMNIST(0,[class1,class2]);
+
+% Select number of hidden units
+nh = 2;
+
+% Train the autoencoder
+myAutoencoder = trainAutoencoder(training_set',nh);
+myEncodedData = encode(myAutoencoder,training_set');
+
+% Plot encoded data
+plotcl(myEncodedData',training_class)
+
+
